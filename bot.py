7215af5d321ef1b9ff95a87341388a824612d33a
Yerassil <yerassil@gmail.com>
2020-02-22 20:44:33 +0600

    Add simple dialogue response

--- bot.py
+++ bot.py
@@ -9,4 +9,16 @@ def start_handler(message):
     bot.send_message(message.chat.id, 'Hi there!')
 
 
+@bot.message_handler(content_types=["text"])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if text == 'hi':
+        bot.send_message(chat_id, 'Hi there! I\'m a parser bot')
+    elif text == 'how are you?':
+        bot.send_message(chat_id, 'I\'m fine. How are you?')
+    else:
+        bot.send_message(chat_id, 'Sorry, didn\'t catch that')
+
+
 bot.polling(none_stop=True)
