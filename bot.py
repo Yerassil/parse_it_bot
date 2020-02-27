2f2acf52bb8d2d2c9447e02cf287ea683262f0c0
Yerassil <yerassil@gmail.com>
2020-02-27 22:45:30 +0600

    Add askAge functionality

--- bot.py
+++ bot.py
@@ -3,10 +3,27 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
+isRunning = 0
+
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
-    bot.send_message(message.chat.id, 'Hi there!')
+    global isRunning
+    if not isRunning:
+        chat_id = message.chat.id
+        msg = bot.send_message(chat_id, 'How old are you?')
+        bot.register_next_step_handler(msg, askAge)
+        isRunning = True
+
+
+def askAge(message):
+    chat_id = message.chat.id
+    text = message.text
+    if not text.isdigit():
+        msg = bot.send_message(chat_id, 'Age must be a number. Please, try again.')
+        bot.register_next_step_handler(msg, askAge)
+        return
+    msg = bot.send_message(chat_id, 'Thank you. Now I know that you are ' + text)
 
 
 @bot.message_handler(content_types=["text"])
